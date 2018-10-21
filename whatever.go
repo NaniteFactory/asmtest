fe2d2a3c6664c643914d1b69650ae142dbcaf1cb
Nanite Factory <nanite@naver.com>
2018-10-22 02:20:42 +0900

    another function

--- whatever.go
+++ whatever.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 func add(x, y int64) int64
+func IsZero(ptrppap uintptr) bool
 
 func main() {
 	fmt.Println(add(2, 3))
+	fmt.Println(IsZero(0))
+	fmt.Println(IsZero(2))
 }
--- whatsoever.s
+++ whatsoever.s
@@ -4,3 +4,14 @@ TEXT ·add(SB), $0
 	ADDQ BP, BX
 	MOVQ BX, ret+16(FP)
 	RET
+
+TEXT ·IsZero(SB), $0
+	MOVQ	ppap+0(FP), AX
+	TESTQ	AX, AX
+	JE		true
+false:
+	MOVB	$0, ret+8(FP)
+	RET
+true:
+	MOVB	$1, ret+8(FP)
+	RET
